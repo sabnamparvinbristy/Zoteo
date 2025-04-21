4699a70b23e2047aa9bc5cace72dd1e5dec7e769
sabnamparvinbristy <parvin.sabnam.002@gmail.com>
2025-04-21 23:25:09 +0600

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// mongoose.connect(""mongodb+srv://zoteo:ptesqA2SKlHaMwMn@cluster0.qirmdd0.mongodb.net/"");
+// password->ptesqA2SKlHaMwMn   ,username:zoteo
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to mongo');
+}).catch((err)=> {
+    console.log(err);
+
+})
+
+// password->ptesqA2SKlHaMwMn   ,username:zoteo
+
 const app= express();
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
