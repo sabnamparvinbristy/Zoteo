4951d4de18b2e473135c22421e824e03bb963174
sabnamparvinbristy <parvin.sabnam.002@gmail.com>
2025-05-07 22:13:33 +0600

    update the header & make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,28 @@
-import {BrowserRouter,Routes,Route}  from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/Signin'
-import SignUp from './pages/SignUp'
-import About from './pages/about'
-import Profile from './pages/profile'
-import Header from './components/Header'
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
 
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
 
-        
-        
+        {/* Protecting the /profile route */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
